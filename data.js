f3917239acdb5acca94469d9e91d4d33219f5614
Cigin777 <ciginsk0007@gmail.com>
2026-05-13 16:47:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cutis-cabin-option-1",
+      "name": "CUTIS CABIN OPTION 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.004617381056277736,
+        "pitch": 0.015446716500832736,
+        "fov": 1.1243936730866708
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04199402299254906,
+          "pitch": 0.15986942949076877,
+          "rotation": 0,
+          "target": "1-cutis-cabin-option-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cutis-cabin-option-2",
+      "name": "CUTIS CABIN OPTION 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.003119257914519835,
+        "pitch": 0.051404014954698596,
+        "fov": 1.1579821225479479
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0184070923402615,
+          "pitch": 0.17911569714948783,
+          "rotation": 0,
+          "target": "0-cutis-cabin-option-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "CUTIS CABIN",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
